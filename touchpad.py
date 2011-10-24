6d2b0c9552d10992cdb1e8aa3cb81c7692351926
Matthew Todd <matcatprg@yahoo.com>
2011-10-24 11:48:09 -0700

    added licensinsing information
    GNU GPL v.3.0

--- touchpad.py
+++ touchpad.py
@@ -11,6 +11,19 @@ Uses xinput (of the xorg variety), which should be installed on most/all linux o
 @note
 Done in python b/c my bash script was starting to get complicated, and I like python more.
 
+@license
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @author Matthew Todd
 @date Oct 22, 2011
